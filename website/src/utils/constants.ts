defbb799ab4c188e34752259953a3ee5e67c1e15
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2023-04-20 23:34:46 -0400

    finishing touch

--- website/src/utils/constants.ts
+++ website/src/utils/constants.ts
@@ -1,4 +1,2 @@
-const STAGE = 'v1';
-
 export const SERVER_URL =
-  'https://92ik35c9hc.execute-api.us-east-2.amazonaws.com' + '/' + STAGE;
+  'https://iailfwyra0.execute-api.us-east-2.amazonaws.com';
